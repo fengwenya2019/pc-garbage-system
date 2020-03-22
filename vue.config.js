0884ca77ffb7c1bf3457d0c03f8848ee32eaf9d2
wenya.fwy <wenya.fwy@dtwave-inc.com>
2020-03-22 12:38:32 +0800

    创建页面

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   devServer: {
     proxy: {
       "/laji": {
-        target: "http://192.168.58.49:8080/",
+        target: "http://127.0.0.1:8282/",
         changeOrigin: true,
         ws: true,
         pathRewrite: {
